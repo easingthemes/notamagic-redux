0d408e779908b1cbdae6f9c32d89537e9ccde57c
Dragan Filipovic <dragan.filipovic@netcentric.biz>
2017-10-16 01:57:17 +0200

    [build] update build

--- manifest.php
+++ manifest.php
@@ -1,6 +1,6 @@
 <?php
 class Manifest {
   public $css = '/build/static/css/main.619c1818.css';
-  public $js = '/build/static/js/main.79c5fafd.js';
+  public $js = '/build/static/js/main.edb506a6.js';
 }
 ?>
